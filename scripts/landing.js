32472a1a3e0bd635b26938057c27c240f2f480bc
Craig Whitfield <Craig Whitfield>
2017-04-12 17:51:21 -0600

    Problem solved and fixed checkpoint 9, final commit

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,29 @@
-var animatePoints = function() {
+var animatePoints = function () { 
+    var points = document.getElementsByClassName('point');
  
-var points = document.getElementsByClassName('point');
- 
-var revealPoint = function (index) {
+    var revealPoint = function (index) {
         points[index].style.opacity = 1;
         points[index].style.transform = "scaleX(1) translateY(0)";
         points[index].style.msTransform = "scaleX(1) translateY(0)";
         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-  };
+    };
 
-for (var i = 0; i < 3; i++){
- 	revealPoint(i);
-}
-
-window.onload = function () {
-    
-    if (window.innerHeight > 950){
-        animatePoints(pointsArray);
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
     }
-    
-    var sellingPoints = document.getElementByClassName('selling-points')[0];
+};
+
+window.onload = function () { 
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
-    window.addEventListener('scroll', function(event){
+    if (window.innerHeight > 950) {
+        animatePoints();
+    }
+    
+    window.addEventListener('scroll', function(event) {
        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-           animatePoints(pointsArray);
+           animatePoints();
        }
     });
 };
